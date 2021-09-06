cecbab3248af6c60c547b7116dec8416ac5a4a95
Kamil Tarka <kamiltarka5@gmail.com>
2021-09-06 18:11:58 +0200

    Seeder

---
+++ FvManagerApi/FvManagerSeeder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FvManagerApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FvManagerApi
+{
+    public class FvManagerSeeder
+    {
+        private readonly FvManagerDbContext _dbContext;
+
+        public FvManagerSeeder(FvManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            if(_dbContext.Database.CanConnect())
+            {
+                var pendingMIgrations = _dbContext.Database.GetPendingMigrations();
+
+                if (pendingMIgrations != null && pendingMIgrations.Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+
+                if(!_dbContext.Role.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Role.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                }
+            };
+
+            return roles;
+        }
+    }
+}
--- FvManagerApi/Startup.cs
+++ FvManagerApi/Startup.cs
@@ -31,6 +31,7 @@ namespace FvManagerApi
 
             services.AddControllers();
             services.AddDbContext<FvManagerDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("FvManagerDbConnection")));
+            services.AddScoped<FvManagerSeeder>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FvManagerApi", Version = "v1" });
@@ -38,16 +39,17 @@ namespace FvManagerApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, FvManagerSeeder fvManagerSeeder)
         {
+            fvManagerSeeder.Seed();
+            app.UseHttpsRedirection();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FvManagerApi v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
